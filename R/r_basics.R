f49352b1e6925c35b6d3d1c097331344c9c4622e
hannahjohnson71 <hannahljohnson21@gmail.com>
2024-09-10 15:18:06 -0700

    added operators

--- R/r_basics.R
+++ R/r_basics.R
@@ -11,6 +11,54 @@ TRUE
 FALSE
 
 # Operators ----
+  # Arithmetic
+2 + 3 # note the spacing
+2+3 # hard to read, difficult for future review
+2 - 3
+2 * 3 # multiplication
+2 ** 3 # 2 to the 3rd
+2 ^ 3 # better way to use an exponent
+2 / 3 # division
+
+  # Comparison
+2 == 2 # tests for equality
+"Joe" == "joe" # case-sensitivity
+"Joe" == 'Joe' # not quotation-sensitive
+# try to use double-quotes in R , as single quote can be mistaken for back-tick
+2 == 1 + 1 # OK
+2 == (1 + 1) # better
+
+2 != 1 # tests inequality
+
+2 < 3
+2 > 3
+2 <= 2
+2 >= 3
+
+# somewhat of a cruel joke
+TRUE == 1 # Boolean value of true is 1
+isTRUE(TRUE == 1)
+FALSE == 0 # Boolean value of true is 0
+isTRUE(FALSE == 0)
+
+isTRUE(TRUE) # function testing if the argument is literally TRUE
+isTRUE(1) # function testing if the argument is literally TRUE
+?isTRUE # queries built-in help
+
+2 < 3 & 2 > 3 # both have to pass to return TRUE
+2 < 3 | 2 > 3 # either can be TRUE to return TRUE
+2 < 3 & (2 == 1 | 2 ==2) # grouping logic to test multiple conditions
+
+# type matters (sometimes)
+"joe" # string or character type
+typeof("joe")
+42 # numeric type (double precision, floating point)
+typeof(42)
+TRUE # logical or Boolean type
+typeof(TRUE)
+
+42 == "42" # this is TRUE, b/c equality can cross types
+identical(42, "42") # type matters for identity
 
 # variables ----
 
