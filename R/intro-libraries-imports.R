565236ce711ed2af0a98ee1c44756dcdaa98c9b5
hannahjohnson71 <hannahljohnson21@gmail.com>
2024-09-11 11:08:02 -0700

    dataframes, pipes, filters

--- R/intro-libraries-imports.R
+++ R/intro-libraries-imports.R
@@ -10,8 +10,34 @@ nrow(sta_meta) # count number of rows
 ncol(sta_meta) # count number of columns
 summary(sta_meta) # summary of data, can get some info on NA's
 
-# Using Import shortcut in Environment window to read in xlsx and 
+# Using Import shortcut in Environment window to read in xlsx and
 # copy/paste code here in the R file
 library(readxl)
 icebreaker_answers <- read_excel("data/icebreaker_answers.xlsx")
 View(icebreaker_answers)
+
+library(dplyr)
+
+# Make new dataset named odot_meta, pulling data from sta_meta where
+# the agency is ODOT and the highway is I-5 (id = 1).
+odot_meta <- sta_meta |> # old pipe notation is %>%, use new |>
+  filter(agency == "ODOT"
+         , highwayid == 1)
+
+
+notodot_meta <- sta_meta |>
+  filter(
+    agency != "ODOT"
+  )
+
+# look for n/a values
+nas_meta <- sta_meta |>
+  filter(
+    is.na(detectorlocation) # is.na is to find null values
+  )
+
+# exlude n/a values
+real_meta <- sta_meta |>
+  filter(
+    !is.na(detectorlocation) # !is.na finds everything that is not null
+  )
