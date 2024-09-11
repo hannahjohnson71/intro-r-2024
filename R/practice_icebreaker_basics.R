04fc8a8bb73b1c4565fe3727a1b979bc58c04670
hannahjohnson71 <hannahljohnson21@gmail.com>
2024-09-11 12:02:46 -0700

    add comments for icebreaker practice answers

--- R/practice_icebreaker_basics.R
+++ R/practice_icebreaker_basics.R
@@ -9,18 +9,22 @@ library(dplyr)
 icebreaker_stuff <- read_excel("data/icebreaker_answers.xlsx")
 
 # Take a peek!
-View(icebreaker_stuff)
+View(icebreaker_stuff) # you can also just type in the name of the df
 colnames(icebreaker_stuff)
 
 # Create a travel_speed column in your data frame using vector operations and 
 #   assignment
-icebreaker_stuff$travel_speed <- icebreaker_stuff$travel_distance / (icebreaker_stuff$travel_time / 60)
+icebreaker_stuff$travel_speed <- icebreaker_stuff$travel_distance / 
+  (icebreaker_stuff$travel_time / 60)
 
 # Look at a summary of the new variable--seem reasonable?
 summary(icebreaker_stuff$travel_speed)
+boxplot(icebreaker_stuff$travel_speed ~ icebreaker_stuff$travel_mode) # quick boxplot
+hist(icebreaker_stuff$travel_speed) # quick histogram
 
 # Choose a travel mode, and use a pipe to filter the data by your travel mode
-filter(icebreaker_stuff, icebreaker_stuff$travel_mode == "car")
+filter(icebreaker_stuff, icebreaker_stuff$travel_mode == "car") # method 1
+icebreaker_stuff |> filter(travel_mode == "car") # method 2
 
 # Note the frequency of the mode (# of rows returned)
 nrow(filter(icebreaker_stuff, icebreaker_stuff$travel_mode == "car"))
@@ -40,8 +44,9 @@ filter(car_data, car_data$travel_time > 100)
 
 # Stretch yourself: Repeat the above, but this time filter the data by two 
 #   travel modes (Hint: %in%)
-modes_list <-list("car", "bus")
+modes_list <-list("car", "bus") # method 1, step 1: make list of values
 carandbus_data <- icebreaker_stuff |>
   filter(
      icebreaker_stuff$travel_mode %in% modes_list
-  )
+  ) # method 1, step 2: check for value in list
+icebreaker_stuff |> filter(travel_mode %in% c("car", "bus")) # method 2
