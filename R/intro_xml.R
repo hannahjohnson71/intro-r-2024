7a7968007c80ce3584a19fb1e277c1386d4a02b4
hannahjohnson71 <hannahljohnson21@gmail.com>
2024-09-12 16:32:49 -0700

    unnesting xml file

--- R/intro_xml.R
+++ R/intro_xml.R
@@ -6,5 +6,15 @@ library(readr)
 # read in xml data for wsdot stations metadata
 meta_xml <- as_list(read_xml("https://wsdot.wa.gov/Traffic/WebServices/SWRegion/Service.asmx/GetRTDBLocationData"))
 
-meta_df <- as_tibble(meta_xml) %>%
+meta_df <- as_tibble(meta_xml) |>
   unnest_longer(RTDBLocationList)
+
+meta_unnest_df <- meta_df |>
+  filter(RTDBLocationList_id == "RTDBLocation") |>
+  unnest_wider(RTDBLocationList)
+
+# continue unpacking it, as necessary
+meta_unnest_more <- meta_unnest_df %>%
+  unnest(cols = names(.)) %>%
+  unnest(cols = names(.)) %>%
+  type_convert()
